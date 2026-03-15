5d17b7176462f93a6c1994593bad62373749ae86
choterifa <choterifa@gmail.com>
2026-03-15 14:54:23 -0600

    Initial Commit

---
+++ Promtier/PromtierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromtierApp.swift
+//  Promtier
+//
+//  Created by Carlos on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PromtierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
